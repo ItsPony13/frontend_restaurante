6e75c186b1c02e234bfdd78fd8fca4e826d86375
Juan Castillo <castillorodriguesjuanjose@gmail.com>
2025-10-06 15:12:23 -0600

    Cambios en api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 //Se pone la ip de la maquina local donde se corre el backend
 //para ver la ip de tu maquina local corre ipconfig en cmd (windows) o ifconfig en mac/linux
 const API = axios.create({
-  baseURL: "http://192.168.100.6:3000",
+  // baseURL: "http://192.168.100.6:3000",
+    baseURL:"http://10.229.179.7:3000",
   headers: {
     "Content-Type": "application/json",
   },
